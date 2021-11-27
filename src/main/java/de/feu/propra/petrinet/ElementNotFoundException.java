76f9fdcfe4a627f90c4aa711b1cd784fb61296b6
j-hap <ju.hapke@gmail.com>
2021-11-27 09:59:43 +0000

    javadoc

--- src/main/java/de/feu/propra/petrinet/ElementNotFoundException.java
+++ src/main/java/de/feu/propra/petrinet/ElementNotFoundException.java
@@ -1,5 +1,11 @@
 package de.feu.propra.petrinet;
 
+/**
+ * Exception to be thrown when a searched object is not found.
+ * 
+ * @author j-hap 
+ *
+ */
 public class ElementNotFoundException extends Exception {
   private static final long serialVersionUID = 1L;
 }
