dd0cd610bcace45fbadf60a8b1c85060e7305ec8
j-hap <ju.hapke@gmail.com>
2021-11-27 09:52:44 +0000

    javadoc

--- src/main/java/de/feu/propra/petrinet/ArcPlan.java
+++ src/main/java/de/feu/propra/petrinet/ArcPlan.java
@@ -1,10 +1,22 @@
 package de.feu.propra.petrinet;
 
+/**
+ * POD class to temporarily store information about an {@code Arc} defined in a
+ * PNML file.
+ * 
+ * @author j-hap 
+ *
+ */
 public class ArcPlan {
   public final String id;
   public final String sourceId;
   public final String targetId;
 
+  /**
+   * @param id       Unique ID of the {@code Arc} to create.
+   * @param sourceId Unique ID of the source {@code PetriNode}.
+   * @param targetId Unique ID of the target {@code PetriNode}.
+   */
   public ArcPlan(String id, String sourceId, String targetId) {
     this.id = id;
     this.sourceId = sourceId;
