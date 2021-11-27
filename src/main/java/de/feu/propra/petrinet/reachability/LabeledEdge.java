9cf61ffc9efd2c83e3f6ba5ff7f85a4eb927886c
j-hap <ju.hapke@gmail.com>
2021-11-27 11:42:05 +0000

    removed unused classes

--- src/main/java/de/feu/propra/petrinet/reachability/LabeledEdge.java
+++
@@ -1,9 +0,0 @@
-package de.feu.propra.petrinet.reachability;
-
-public class LabeledEdge extends GenericLabeledEdge<Marking, Marking> {
-
-	public LabeledEdge(Marking from, Marking to, String l) {
-		super(from, to, l);
-	}
-
-}
