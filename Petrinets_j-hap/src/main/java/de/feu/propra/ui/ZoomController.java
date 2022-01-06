a727489e5b322199e2ce1b82bf67b1396bec1659
j-hap <ju.hapke@gmail.com>
2022-01-06 15:50:34 +0000

    improved zoom behavior to that mouse stays on same position relative to graph

--- Petrinets_j-hap/src/main/java/de/feu/propra/ui/ZoomController.java
+++ Petrinets_j-hap/src/main/java/de/feu/propra/ui/ZoomController.java
@@ -7,12 +7,16 @@ import org.graphstream.ui.view.View;
 
 /**
  * Util class to handle MouseWheel zoom on a GraphStream graph {@code View}.
+ * Increases / Decreases Zoom by 10% in every scroll increment. The zoom center
+ * is moved so that the mouse position stays the same in respect to the graph.
+ * 
+ * @see <a href=
+ *      "https://stackoverflow.com/questions/44675827/how-to-zoom-into-a-graphstream-view">https://stackoverflow.com/questions/44675827/how-to-zoom-into-a-graphstream-view</a>
  * 
  * @author j-hap 
- *
  */
 public class ZoomController implements MouseWheelListener {
-  private static final double zoomIncrement = 0.1;
+  private static final double baseFactor = 1.1;
 
   /**
    * Increases zoom on mousewheel up rotation, decreases zoom on mousewheel down
@@ -22,8 +26,12 @@ public class ZoomController implements MouseWheelListener {
   public void mouseWheelMoved(MouseWheelEvent e) {
     var view = (View) e.getComponent();
     var direction = e.getWheelRotation();
-    var zoomLevel = view.getCamera().getViewPercent();
-    zoomLevel = Math.max(0, zoomLevel + zoomIncrement * direction);
-    view.getCamera().setViewPercent(zoomLevel);
+    var zoomFactor = Math.pow(baseFactor, direction);
+    var cam = view.getCamera();
+    var zoomLevel = cam.getViewPercent() * zoomFactor;
+    var mousePositionInGraphUnits = cam.transformPxToGu(e.getX(), e.getY());
+    var newCenter = mousePositionInGraphUnits.interpolate(cam.getViewCenter(), zoomFactor);
+    cam.setViewCenter(newCenter.x, newCenter.y, newCenter.z);
+    cam.setViewPercent(zoomLevel);
   }
 }
